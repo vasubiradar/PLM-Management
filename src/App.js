d4c56ac6185c83f3e14c5907ee0c9f1a48171285
VasudhaBiradar <vasudha.biradar@mitaoe.ac.in>
2025-02-12 22:35:44 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './HomePage/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Hello</h1>
+      <h1>this is me ;</h1>
+      <h2>e-commerce</h2>
+      <h1>hi</h1>
+      <h1>vaishu</h1>
+      <h1>prithvi</h1> */}
+      <Nav></Nav>
+      <h1>prithvi</h1>
+      <h1>Gargiiiii Hereeeee</h1>
+      <h1> check check check hellooooo</h1>
+      <h1> ved shlok </h1>
+      
+    </div>
+  );
+}
+
+export default App;
